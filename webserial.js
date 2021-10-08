c52e7b8f8db1f2bf3e5d4dc13bd8dfbf9049f239
Jesper Lindstrøm Nielsen <jesper.jln@gmail.com>
2021-10-08 20:48:43 +0200

    wip

--- webserial.js
+++ webserial.js
@@ -147,6 +147,7 @@ class Transport {
     }
 
     rawRead = async ({timeout=0} = {}) => {
+        let t;
         let reader = this.device.readable.getReader();
         let done = false;
         let value = new Uint8Array(0);
